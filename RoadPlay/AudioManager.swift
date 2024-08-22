434ff30c60ec13d9bd3d4465b07ec8954b8cf7ae
albertcai101 <albertcai.paloalto@gmail.com>
2024-08-22 12:22:23 -0700

    now playing notification

--- RoadPlay/AudioManager.swift
+++ RoadPlay/AudioManager.swift
@@ -5,8 +5,9 @@
 //  Created by Albert Cai on 8/22/24.
 //
 
-import SwiftUI
 import AVFoundation
+import MediaPlayer
+import SwiftUI
 
 class AudioManager: ObservableObject {
     struct Track: Hashable {
@@ -23,6 +24,9 @@ class AudioManager: ObservableObject {
     @Published var isPlaying = true
     
     init() {
+        configureNowPlaying()
+        configureRemoteCommandCenter()
+        
         let trackNames = ["piano", "bass", "vocals", "drums", "guitar", "other"]
         let initialVolumes: [Float] = [0.5, 0.7, 0.8, 0.6, 0.9, 0.4]
         
@@ -32,9 +36,12 @@ class AudioManager: ObservableObject {
                 player.numberOfLoops = -1 // Loop indefinitely
                 player.play()
                 
-                tracks.append(Track(name: name, volume: initialVolumes[index], player: player))
+                let track = Track(name: name, volume: initialVolumes[index], player: player)
+                tracks.append(track)
             }
         }
+        
+        updateNowPlayingInfo()
     }
     
     func createPlayer(for name: String) -> AVAudioPlayer? {
@@ -55,20 +62,67 @@ class AudioManager: ObservableObject {
         if let index = tracks.firstIndex(where: { $0.name == name }) {
             tracks[index].volume = newVolume
             tracks[index].player?.volume = newVolume
+            updateNowPlayingInfo()
         }
     }
     
     func togglePlayPause() {
-            isPlaying.toggle()
-            
-            if isPlaying {
-                for track in tracks {
-                    track.player?.play()
-                }
-            } else {
-                for track in tracks {
-                    track.player?.pause()
-                }
+        isPlaying.toggle()
+        
+        if isPlaying {
+            for track in tracks {
+                track.player?.play()
+            }
+        } else {
+            for track in tracks {
+                track.player?.pause()
             }
         }
+        
+        updateNowPlayingInfo()
+    }
+    
+    private func configureNowPlaying() {
+        var nowPlayingInfo = [String: Any]()
+        nowPlayingInfo[MPMediaItemPropertyTitle] = "Live Session"
+        nowPlayingInfo[MPNowPlayingInfoPropertyIsLiveStream] = true
+        MPNowPlayingInfoCenter.default().nowPlayingInfo = nowPlayingInfo
+    }
+    
+    private func updateNowPlayingInfo() {
+        var nowPlayingInfo = MPNowPlayingInfoCenter.default().nowPlayingInfo ?? [String: Any]()
+        nowPlayingInfo[MPNowPlayingInfoPropertyPlaybackRate] = isPlaying ? 1.0 : 0.0
+        
+        // Assuming we're using the first track as a representative title/artist
+        if let firstTrack = tracks.first {
+            nowPlayingInfo[MPMediaItemPropertyTitle] = "Roadplay Session"
+        }
+
+        MPNowPlayingInfoCenter.default().nowPlayingInfo = nowPlayingInfo
+    }
+    
+    private func configureRemoteCommandCenter() {
+        let commandCenter = MPRemoteCommandCenter.shared()
+
+        commandCenter.playCommand.isEnabled = true
+        commandCenter.playCommand.addTarget { [unowned self] event in
+            if !self.isPlaying {
+                self.togglePlayPause()
+                return .success
+            }
+            return .commandFailed
+        }
+
+        commandCenter.pauseCommand.isEnabled = true
+        commandCenter.pauseCommand.addTarget { [unowned self] event in
+            if self.isPlaying {
+                self.togglePlayPause()
+                return .success
+            }
+            return .commandFailed
+        }
+        
+        // Additional controls can be configured similarly:
+        // commandCenter.skipForwardCommand, commandCenter.skipBackwardCommand, etc.
+    }
 }
